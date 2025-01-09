59becd934e324667a85771cdac816fc929d19bbc
SolidDavid03 <ficocellidavide4@gmail.com>
2025-01-09 11:50:24 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e9f86847-fb08-42d0-8d86-b1a4119e4dc0';
+export const MODAL_CLOSE_SEC = 2.5;
